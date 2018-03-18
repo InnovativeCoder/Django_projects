c891f838eb035804416705451e1e3e37ce123d4c
innovative coder <aashitsingh2010@gmail.com>
2018-03-18 13:35:56 +0530

    Done with project 04

--- 04projectfour/website/articles/urls.py
+++ 04projectfour/website/articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-
-    path('', view)
+    path('', views.ArticleListView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_page'),
 ]
+#-pk is primary key 
--- 04projectfour/website/articles/views.py
+++ 04projectfour/website/articles/views.py
@@ -1,8 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 from .models import Article
 
-class ArticlListView(ListView):
+class ArticleListView(ListView):
     model = Article
     template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'detail.html'
+    context_object_name = 'batman'
